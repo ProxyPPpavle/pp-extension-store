f38f03a377a3704581098f3b8003df5e1a913265
ProxyPPpavle <pavlemaster6@gmail.com>
2026-02-07 18:46:15 +0100

    Update sw.js with specific PropellerAds configuration

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,6 @@
-/* PropellerAds Service Worker */
-importScripts('https://asyncloader.com/sw/3.0.0/loader.js');
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10582442
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
