4039a25fa5629ae76c621cab5843b75cea5102b5
ProxyPPpavle <pavlemaster6@gmail.com>
2026-02-11 22:51:05 +0100

    Forcing rebuild to update privacy page config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    // Building for production
     build: {
         rollupOptions: {
             input: {
